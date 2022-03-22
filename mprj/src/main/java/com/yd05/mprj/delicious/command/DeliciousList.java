82f8bb00a7709f5f587f9a605ddc9d5014d9e38d
been <jybrose@hanmail.net>
2022-03-22 23:43:30 +0900

    delicious 수정

---
+++ mprj/src/main/java/com/yd05/mprj/delicious/command/DeliciousList.java
@@ -0,0 +1,17 @@
+package com.yd05.mprj.delicious.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yd05.mprj.comm.Command;
+
+public class DeliciousList implements Command {
+
+	@Override
+	public String excute(HttpServletRequest request, HttpServletResponse response) {
+		// 맛집 목록
+		
+		return "spot/deliciousList.tiles";
+	}
+
+}
