bb6c1abdf398a2122e597deba99b5ecdc95cc3b3
dms6800 <msdo6800@gmail.com>
2022-03-30 14:54:03 +0900

    Inconsistent merge state: could not find file MERGE_MSG in .git folder. This file contains the commit message for a merge commit.

---
+++ mprj/src/main/java/com/yd05/mprj/web/WeatherView.java
@@ -0,0 +1,16 @@
+package com.yd05.mprj.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yd05.mprj.comm.Command;
+
+public class WeatherView implements Command {
+
+	@Override
+	public String excute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
